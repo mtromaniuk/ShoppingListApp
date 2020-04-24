61b40941c0d7e1144477f2df2cb44eafc4c8d43a
Mateusz Romaniuk <mt.romaniuk@gmail.com>
2020-04-24 23:25:15 +0200

    Completed project

---
+++ shoppingListApp.js
@@ -0,0 +1,78 @@
+var vm = new Vue({
+    el: "#app",
+    data() {
+        return {
+            inputDataModel: '',
+            products: [],
+            filterShowBought: null,
+            sortAllAsscending: true
+        }
+    },
+    methods: {
+        addNewProduct() {
+            this.products.push({
+                name: this.inputDataModel,
+                price: 0,
+                isChecked: false,
+                productBought: false,
+            });
+
+            this.inputDataModel = '';
+        },
+        deleteItem(index) {
+            this.products.splice(index, 1);
+        },
+        sortAllNames() {
+            if (this.sortAllAsscending == true) {
+                this.products.name = this.products.sort((a, b) => a.name > b.name ? 1 : -1);
+
+                this.sortAllAsscending = !this.sortAllAsscending;
+            } else {
+                this.products.name = this.products.sort((a, b) => a.name < b.name ? 1 : -1);
+
+                this.sortAllAsscending = !this.sortAllAsscending;
+            }
+        },
+
+        sortAllPrices() {
+            if (this.sortAllAsscending == true) {
+                this.products.price = this.products.sort((a, b) => a.price > b.price ? 1 : -1);
+
+                this.sortAllAsscending = !this.sortAllAsscending;
+            } else {
+                this.products.price = this.products.sort((a, b) => a.price < b.price ? 1 : -1);
+
+                this.sortAllAsscending = !this.sortAllAsscending;
+            }
+        },
+
+        shouldProductDisplay(productBought) {
+            if (this.filterShowBought === null) {
+                return true;
+            }
+            return this.filterShowBought === productBought;
+        }
+    },
+    computed: {
+        calculatePrice() {
+            const calculatedPrice = this.products.reduce((acc, product) => {
+                if (product.productBought) {
+                    return acc + parseInt(product.price);
+                }
+                return acc;
+            }, 0);
+
+            return calculatedPrice;
+        },
+        listOfCheckedElement() {
+            const lsitOfElement = this.products.reduce((acc, product) => {
+                if (product.productBought) {
+                    return acc + 1;
+                }
+                return acc;
+            }, 0);
+            
+            return lsitOfElement;
+        }
+    }
+})
